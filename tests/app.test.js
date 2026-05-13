61b1a13ddca06a00e450d68b1ef83ab8f0c18eea
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2026-05-13 18:24:36 -0300

    new teste

--- tests/app.test.js
+++ tests/app.test.js
@@ -22,6 +22,7 @@ describe('GET /users', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body[0].name).toBe('Alice');
     expect(response.body[0].email).toBe('alice@example.com');
+    expect(response.body[0].endereco).toBe('teste');
   });
 
   it('Deve retornar lista vazia quando não há usuários', async () => {
